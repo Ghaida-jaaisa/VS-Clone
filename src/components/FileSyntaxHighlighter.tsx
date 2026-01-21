e2c0df4a28d0465e9f61b4f298e381ba85702b71
Ghaida Ja'aysah <ghaida.20004@gmail.com>
2026-01-21 19:44:57 +0200

    fix: Fix react-syntax-highlighter importing

--- src/components/FileSyntaxHighlighter.tsx
+++ src/components/FileSyntaxHighlighter.tsx
@@ -1,14 +1,16 @@
-import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
 interface IProps {
-  content: string | undefined;
+  content?: string;
 }
 
-const FileSyntaxHighlighter = ({ content }: IProps) => {
+const FileSyntaxHighlighter = ({ content = "" }: IProps) => {
   return (
     <SyntaxHighlighter
       language="javascript"
       style={atomOneDark}
+      showLineNumbers
       customStyle={{
         backgroundColor: "transparent",
         width: "100%",
@@ -16,9 +18,8 @@ const FileSyntaxHighlighter = ({ content }: IProps) => {
         overflowX: "auto",
         fontSize: "1.5rem",
       }}
-      showLineNumbers
     >
-      {String(content)}
+      {content}
     </SyntaxHighlighter>
   );
 };
